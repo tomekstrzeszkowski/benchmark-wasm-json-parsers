e2176981e3b839e86c8a5d4310ed38f8e3dd6876
tomekstrzeszkowski <tomasz.strzeszkowski@dashhudson.com>
2022-08-14 13:48:46 +0200

    Export rust parser

--- cmd/wasm-rust/src/lib.rs
+++ cmd/wasm-rust/src/lib.rs
@@ -1,7 +1,7 @@
 mod utils;
 mod car;
+use car::parse_json;
 use wasm_bindgen::prelude::*;
-use chrono::{Utc, TimeZone};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -10,22 +10,6 @@ use chrono::{Utc, TimeZone};
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greets() {
-    let car = car::Car{
-        name: String::from("Honda"),
-        miles_per_galon: 12.6,
-        displacement: None,
-        horsepower: 140,
-        weight_in_lbs: 1900,
-        cylinders: 4,
-        year: Some(Utc.ymd(1970, 1, 1)),
-        acceleration: 100,
-    };
-    let label = &format!("Work in progress, wasm-rust {}!", car.name);
-    alert(label);
+pub fn rust_parse_json(js_value: &str) -> String {
+    return parse_json(js_value);
 }
